1d3052d3da395640b349e2fc5448e8724d67f7c2
jitinaware <60826351+jitinaware@users.noreply.github.com>
2022-05-11 20:49:51 -0400

    test

--- aws.tf
+++ aws.tf
@@ -64,7 +64,7 @@ resource "aws_security_group" "aws-vm" {
 
 resource "aws_instance" "aws-vm" {
   subnet_id = local.vpc_pubsubnet_id
-  ami = "ami-011b910522db6cac9"
+  ami = "ami-0efe8dcd6e370e0fc"
   instance_type = var.aws_instance_type
   key_name = var.aws_keyname
 
